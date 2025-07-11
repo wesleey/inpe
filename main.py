3647312a351d25beeabf45f945d6e9f492505be0
Wesley Barbosa <wesley.barbosa.developer@gmail.com>
2025-07-11 17:14:21 -0300

    Add workflows

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
-import pandas as pd
+import os
 import datetime
+import pandas as pd
 
-d = datetime.datetime.now(datetime.UTC)
-url = (f"https://dataserver-coids.inpe.br/queimadas/queimadas/focos/csv/10min/"
-       f"focos_10min_{d.strftime('%Y%m%d_%H')}{(d.minute // 10 * 10):02d}.csv")
 
-try:
-    df = pd.read_csv(url)
-    df.to_csv("queimadas.csv", mode='a', header=not pd.io.common.file_exists("queimadas.csv"), index=False)
-    print("CSV atualizado com sucesso.")
-except Exception as e:
-    print(f"Erro ao carregar ou salvar CSV: {e}")
+def main():
+    current_timestamp = get_current_timestamp()
+    last_timestamp = get_last_timestamp()
+
+    if current_timestamp == last_timestamp:
+        return
+
+    d = datetime.datetime.now(datetime.UTC)
+    url = (f"https://dataserver-coids.inpe.br/queimadas/queimadas/focos/csv/10min/"
+        f"focos_10min_{current_timestamp}.csv")
+
+    try:
+        df = pd.read_csv(url)
+        file_exists = os.path.exists('queimadas.csv')
+        df.to_csv('queimadas.csv', mode='a', header=not file_exists, index=False)
+        save_timestamp(current_timestamp)
+    except Exception as e:
+        print(f"Erro: {str(e)}")
+
+
+def get_current_timestamp():
+    d = datetime.datetime.now(datetime.UTC)
+    return f"{d.strftime('%Y%m%d_%H')}{(d.minute // 10 * 10):02d}"
+
+
+def get_last_timestamp():
+    if os.path.exists('.timestamp'):
+        with open('.timestamp', 'r') as f:
+            return f.read().strip()
+    return None
+
+
+def save_timestamp(timestamp):
+    with open('.timestamp', 'w') as f:
+        f.write(timestamp)
+
+
+if __name__ == '__main__':
+    main()
